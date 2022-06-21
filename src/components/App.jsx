085ca70c345a6a59237625c6341f0ebaaac3d284
js-designs-321 <jatni.321@gmail.com>
2022-06-21 22:55:26 +0530

    First Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+// let time = new Date().toLocaleTimeString();
+// console.log(time);
+
+
+function App() {
+
+  
+  return (
+    <div className="container">
+      <h1>TIME</h1>
+      <button>Get Time</button>
+    </div>
+  );
+}
+
+export default App;
